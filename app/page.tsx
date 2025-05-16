93ca6dee6baf8fb74c6ccceded552543b4569741
Anish Gupta <anishhuwu@gmail.com>
2025-05-16 10:12:58 +0530

    02: added pettier plugin for classname sorting

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
-import { useState } from "react";
-
-import Image from "next/image";
 
 export default function Home() {
-  return <div> hello world</div>;
+  return (
+    <div className="flex items-center justify-center bg-sky-900 px-20 py-10 text-2xl font-bold text-sky-100">
+      hello world
+    </div>
+  );
 }
